58b0cd58613ec26286c3f0702e67039c02ec7e16
oxnoctisxo <tip.hon@hotmail.fr>
2017-07-08 14:49:42 +0200

    Services declaration
    Addition of a declaration for the stanize service (allows external images and videos to be safely linked)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StanizerService } from '../services/stanizer.service';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    StanizerService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
