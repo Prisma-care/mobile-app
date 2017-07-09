9bc1a28948ec8e9a526bdfa6d681ebfaf35e5580
Frederik Vincx <frederik.vincx@gmail.com>
2017-07-09 19:02:45 +0200

    Organize images
    Folders for thumbs, small, medium and orginal.
    Thumbs are square 150x150

--- src/pages/stories/stories.ts
+++ src/pages/stories/stories.ts
@@ -17,25 +17,25 @@ export class StoriesPage {
 
 
   public album1Data:Array<any> = [
-      {id:"marieJosE-slide22",src:"assets/img/cHTUaigQQ2iFLSLC34OP_stadhuishalle.jpg"},
-      {id:"marieJosE-slide29",src:"assets/img/peROWMdTyapL2RcptjpQ_hallerbos.jpg"},
-      {id:"marieJosE-slide28",src:"assets/img/LVZpafa9RpCyPNPLYuSn_sabena-security.jpg"},
+      {id:"marieJosE-slide22",src:"assets/img/t/cHTUaigQQ2iFLSLC34OP_stadhuishalle.jpg"},
+      {id:"marieJosE-slide29",src:"assets/img/t/peROWMdTyapL2RcptjpQ_hallerbos.jpg"},
+      {id:"marieJosE-slide28",src:"assets/img/t/LVZpafa9RpCyPNPLYuSn_sabena-security.jpg"},
   ];
 
   public album2Data:Array<any> = [
-      {id:"marieJosE-slide24",src:"assets/img/CBd1zASpTOmZptix3fng_family1.jpg"},
-      {id:"marieJosE-slide25",src:"assets/img/TVRkzCgS7iNdKgM7PSwg_baby.jpg"},
-      {id:"marieJosE-slide26",src:"assets/img/WE6rB9dZRTiIWQlP5qsA_kitten3.jpg"},
+      {id:"marieJosE-slide24",src:"assets/img/t/CBd1zASpTOmZptix3fng_family1.jpg"},
+      {id:"marieJosE-slide25",src:"assets/img/t/TVRkzCgS7iNdKgM7PSwg_baby.jpg"},
+      {id:"marieJosE-slide26",src:"assets/img/t/WE6rB9dZRTiIWQlP5qsA_kitten3.jpg"},
   ];
 
   public album3Data:Array<any> = [
-      {id:"marieJosE-slide223",src:"assets/img/ARSdfjpfSAWvY2J9VpZe_moskow.jpg"},
-      {id:"marieJosE-slide224",src:"assets/img/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
+      {id:"marieJosE-slide223",src:"assets/img/t/ARSdfjpfSAWvY2J9VpZe_moskow.jpg"},
+      {id:"marieJosE-slide224",src:"assets/img/t/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
   ];
 
   public album4Data:Array<any> = [
-      {id:"marieJosE-slide212",src:"assets/img/rJwCxPlrTsCi8xbyzE8m_tasselplay.jpg"},
-      {id:"marieJosE-slide213",src:"assets/img/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
+      {id:"marieJosE-slide212",src:"assets/img/t/rJwCxPlrTsCi8xbyzE8m_tasselplay.jpg"},
+      {id:"marieJosE-slide213",src:"assets/img/t/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
   ];
 
   public albums:Array<any> = [
