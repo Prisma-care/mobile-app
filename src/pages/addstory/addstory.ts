d2ed3392bc1e88d718175aa859ac71ed34485c47
Frederik Vincx <frederik.vincx@gmail.com>
2017-07-10 16:08:55 +0200

    Add action sheet to add a story

--- src/pages/addstory/addstory.ts
+++ src/pages/addstory/addstory.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { Platform, ActionSheetController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-addstory',
-  templateUrl: 'addstory.html'
+	selector: 'page-addstory',
+  	templateUrl: 'addstory.html'
 })
 export class AddStoryPage {
+  constructor(
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController
+  ) { }
 
-  constructor(public navCtrl: NavController) {
-
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Verhaal toevoegen',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Foto nemen',
+          icon: 'camera',
+          handler: () => {
+            console.log('Take photo');
+          }
+        },
+        {
+          text: 'Foto kiezen uit galerij',
+          icon: 'images',
+          handler: () => {
+            console.log('Browse gallery');
+          }
+        },
+      ]
+    });
+    actionSheet.present();
   }
-
 }
