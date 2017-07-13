54504840974cda6a7e178963c1eb604e4c164e45
Thor Galle <thor.galle@student.kuleuven.be>
2017-07-13 10:12:33 +0200

    Story controller update

--- src/pages/stories/stories.ts
+++ src/pages/stories/stories.ts
@@ -21,64 +21,34 @@ import { Album  } from "../../dto/album";
 })
 export class StoriesPage implements OnInit {
 
+  // TODO : remove or do something with this
   public youtubeUrl:string = "www.youtube.com/embed/ERD4CbBDNI0?rel=0&amp;showinfo=0";
   public stanizedYoutubeUrl:any;
-
-
-
-
-  public album1Data:Array<any> = [
-      {id:"marieJosE-slide22",src:"assets/img/t/cHTUaigQQ2iFLSLC34OP_stadhuishalle.jpg"},
-      {id:"marieJosE-slide29",src:"assets/img/t/peROWMdTyapL2RcptjpQ_hallerbos.jpg"},
-      {id:"marieJosE-slide28",src:"assets/img/t/LVZpafa9RpCyPNPLYuSn_sabena-security.jpg"},
-  ];
-
-  public album2Data:Array<any> = [
-      {id:"marieJosE-slide24",src:"assets/img/t/CBd1zASpTOmZptix3fng_family1.jpg"},
-      {id:"marieJosE-slide25",src:"assets/img/t/TVRkzCgS7iNdKgM7PSwg_baby.jpg"},
-      {id:"marieJosE-slide26",src:"assets/img/t/WE6rB9dZRTiIWQlP5qsA_kitten3.jpg"},
-  ];
-
-  public album3Data:Array<any> = [
-      {id:"marieJosE-slide223",src:"assets/img/t/ARSdfjpfSAWvY2J9VpZe_moskow.jpg"},
-      {id:"marieJosE-slide224",src:"assets/img/t/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
-  ];
-
-  public album4Data:Array<any> = [
-      {id:"marieJosE-slide212",src:"assets/img/t/rJwCxPlrTsCi8xbyzE8m_tasselplay.jpg"},
-      {id:"marieJosE-slide213",src:"assets/img/t/C6IS45JTlWcTrL5bnsaw_split-croatia.jpg"},
-  ];
-
-  /* public albums:Array<any> = [
-    {name:"Kindertijd",data:this.album1Data},
-    {name:"Familie & vrienden",data:this.album2Data},
-    {name:"Voor jou geselecteerd",data:this.album3Data},
-    {name:"Relevant vandaag",data:this.album4Data},
-    ];
-    */
 
   user: User;
 
   albums: Album[];
 
-  constructor(public navCtrl: NavController, private stanizerService: StanizerService,
-              private userService: UserService, private storyService: StoryService) {
+  constructor(public navCtrl: NavController, protected stanizerService: StanizerService,
+              protected userService: UserService, protected storyService: StoryService) {
       this.stanizedYoutubeUrl = this.stanizerService.sanitize(this.youtubeUrl);
   }
 
   ngOnInit(): void {
+    /* TESTS: to remove
     this.userService.getUser("12345").toPromise().then(user => {
 
         this.user = user
         console.log("Final user : " + JSON.stringify(this.user));
       });
 
+    this.storyService.getUserStories().toPromise().then(stories =>
+    console.log(JSON.stringify(stories)));
+    */
+
     this.storyService.getAlbums().toPromise().then(albums => {
       this.albums = albums as Album[];
     });
-
-    this.storyService.getUserStories().toPromise().then(stories =>
-    console.log(JSON.stringify(stories)));
   }
 
   getThumb(url: string): string {
