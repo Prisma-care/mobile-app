6a96ed7bdded75a6ed9c34556b7116bbba5515fd
Thor Galle <thor.galle@student.kuleuven.be>
2017-07-27 12:29:37 +0200

    Question shuffle & cycle

--- src/pages/album-detail/album-questions.ts
+++ src/pages/album-detail/album-questions.ts
@@ -29,13 +29,35 @@ export class AlbumQuestions extends AuthGuard implements OnInit {
 
   ngOnInit(): void {
     this.questions = this.questionService.getQuestions(this.query);
+    this.questions = this.shuffle(this.questions);
     this.nextQuestion();
   }
 
+
+  private questionIndex: number = 0;
+
   nextQuestion() {
     // TODO: implement a random question that avoids repetition
-    this.currentQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+    this.currentQuestion = this.questions[this.questionIndex++ % this.questions.length];
   }
 
+  private shuffle(array: string[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 }
