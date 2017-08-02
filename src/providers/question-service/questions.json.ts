d4f98c0c56193addfaec4ef59468659b28b6cc42
Frederik Vincx <frederik.vincx@gmail.com>
2017-08-02 12:42:02 +0200

    Change all questions to themes

--- src/providers/question-service/questions.json.ts
+++ src/providers/question-service/questions.json.ts
@@ -4,14 +4,14 @@ export const QUESTIONS = [
     "questions": [
       "Oude familielden",
       "Land van herkomst",
-      "Overgrootouders?"
+      "Overgrootouders"
     ],
     "keywords": ["grootouders"]
   },
   {
     "questions": [
       "Huis",
-      "Huisdieren?",
+      "Huisdieren",
       "Gewoontes of tradities",
       "Favoriet eten als kind",
       "Kinderkleding"
@@ -39,69 +39,74 @@ export const QUESTIONS = [
       "Eerste verliefdheid",
       "Afspraakjes",
       "Voorstellen aan familie",
-      "Trouwdag?"
+      "Trouwdag"
     ],
     "keywords": ["trouw", "liefde"]
   },
   {
     "questions": [
-      "Wanneer kreeg je je eerste kind?",
-      "Welke namen hebben je kinderen? Heb je nog een foto van vroeger?",
-      "Nam je soms tijd voor jezelf? Wat deed je dan?"
+      "Eerste kind",
+      "Namen van kinderen",
+      "Trouwfeesten",
+      "Geboortes"
     ],
     "keywords": ["familie", "gezin"]
   },
   {
     "questions": [
-      "Wat doe je het liefst in je vrije tijd?",
-      "Ga je graag uit? Met wie en waar dan?",
-      "Kijk je televisie? Wat zijn je lievelingsprogramma's?",
-      "Wat is je favoriete muziek? Ga je soms naar concerten?",
-      "Bespeel je een instrument?"
+      "Vrijdetijdsbezigheden",
+      "Uitgaan",
+      "Lievelingsprogramma's",
+      "Concerten",
+      "Zelf een instrument bespelen"
     ],
     "keywords": ["vrije tijd", "hobby", "hobbies"]
   },
   {
     "questions": [
-      "Had je een beste vriend(in)?",
-      "Wat deed je samen met je vrienden?"
+      "Beste vriend(in)",
+      "Activiteiten samen",
+      "Nieuwe vrienden leren kennen",
+      "Vrienden in kindertijd"
     ],
     "keywords": ["vrienden", "vriendschap", "vriend", "kennis", "kennissen"]
   },
   {
     "questions": [
-      "Herinner je je allereerste vakantie?",
-      "Wat is het verste land waar je al geweest bent?",
-      "Wanneer ging je voor het eerst alleen op vakantie? Waar was dat?",
-      "Op welke manier reisde je vooral?",
-      "Wat is de mooiste stad die je al hebt bezocht?"
+      "Eerste vakantie",
+      "Verre landen",
+      "Alleen op vakantie",
+      "Vervoersmiddelen",
+      "Mooie steden"
     ],
     "keywords": ["vakantie", "reizen", "reis", "avontuur"]
   },
   // MORE GENERAL
   {
     "questions": [
-      "Wie is je favoriete voetballer?",
-      "Beoefen je een sport?",
-      "Welke sport volg je graag?",
-      "Heb je ooit een wedstrijd gewonnen in een sport?"
+      "Favoriete voetballer",
+      "Zelf sporten",
+      "Favoriete sport",
+      "Wedstrijd gewonnen"
     ],
     "keywords": ["sport"]
   },
   {
     "questions": [
-      "Wat is je lievelingsgerecht?",
-      "Eet je soms iets exotisch?",
-      "Wat is je favoriete Italiaanse gerecht?",
-      "Welke Vlaamse kost is jouw favoriet?",
-      "Wat is jouw favoriet snoepgoed?",
+      "lievelingsgerecht",
+      "Exotisch eten",
+      "Italiaanse gerechten",
+      "Vlaamse kost",
+      "Favoriet snoepgoed",
     ],
     "keywords": ["voeding", "eten"]
   },
   {
     "questions": [
-      "Ga je soms op café in je gemeente?",
-      "Herinner je je nog een buurtfeest?"
+      "Buurtcafés",
+      "Buurtfeesten",
+      "Feesten in gemeente",
+      "Evenementen in de gemeente",
     ],
     "keywords": ["gemeente", "roeselare"]
   },
