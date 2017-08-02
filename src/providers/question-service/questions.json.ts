0f06c8b27b47c1bd44797fb5825df41c66fe253c
Frederik Vincx <frederik@prezly.com>
2017-08-02 12:59:44 +0200

    Turn question strings into themes (#64)
    * Turn question strings into themes
    
    * Change all questions to themes
    
    * l -> L

--- src/providers/question-service/questions.json.ts
+++ src/providers/question-service/questions.json.ts
@@ -2,106 +2,111 @@ export const QUESTIONS = [
   // PERSONAL
   {
     "questions": [
-      "Wie was het oudste familielid dat je gekend hebt?",
-      "Van welk land komen je groutouders?",
-      "Kende je je overgrootouders?"
+      "Oude familielden",
+      "Land van herkomst",
+      "Overgrootouders"
     ],
     "keywords": ["grootouders"]
   },
   {
     "questions": [
-      "In wat voor huis woonden jullie?",
-      "Welke namen heb je gekregen? Ben je vernoemd naar iemand?",
-      "Hebben jullie huisdieren?",
-      "Hebben jullie als gezin een speciale gewoonte of traditie?",
-      "Wat at je het liefst?",
-      "Welke kleding droeg je toen?"
+      "Huis",
+      "Huisdieren",
+      "Gewoontes of tradities",
+      "Favoriet eten als kind",
+      "Kinderkleding"
     ],
     "keywords": ["kindertijd", "kind", "jeugd", "gezin"]
   },
   {
     "questions": [
-      "Welk beroep heb je het langst uitgeoefend?",
-      "Waar ging je voor het eerst werken?",
-      "Welke vakantiejobs heb je vroeger gedaan?",
-      "Herinner je je nog een leuke collega?"
+      "Beroep",
+      "Eerste werk",
+      "Vakantiejobs",
+      "Leuke collega's"
     ],
     "keywords": ["werk", "bureau"]
   },
   {
     "questions": [
-      "In welke basisschool zat je? Waarom daar?",
-      "Herinner je je nog een leerkracht van vroeger?"
+      "Basisschool",
+      "Leerkrachten"
     ],
     "keywords": ["school", "leerkracht", "klas", "kindertijd"]
   },
   {
     "questions": [
-      "Kan je je eerste verliedheid nog herinneren?",
-      "Hoe ging dat toen, een liefje hebben?",
-      "Wat kan je je nog herinneren van je trouwdag?"
+      "Eerste verliefdheid",
+      "Afspraakjes",
+      "Voorstellen aan familie",
+      "Trouwdag"
     ],
     "keywords": ["trouw", "liefde"]
   },
   {
     "questions": [
-      "Wanneer kreeg je je eerste kind?",
-      "Welke namen hebben je kinderen? Heb je nog een foto van vroeger?",
-      "Nam je soms tijd voor jezelf? Wat deed je dan?"
+      "Eerste kind",
+      "Namen van kinderen",
+      "Trouwfeesten",
+      "Geboortes"
     ],
     "keywords": ["familie", "gezin"]
   },
   {
     "questions": [
-      "Wat doe je het liefst in je vrije tijd?",
-      "Ga je graag uit? Met wie en waar dan?",
-      "Kijk je televisie? Wat zijn je lievelingsprogramma's?",
-      "Wat is je favoriete muziek? Ga je soms naar concerten?",
-      "Bespeel je een instrument?"
+      "Vrijdetijdsbezigheden",
+      "Uitgaan",
+      "Lievelingsprogramma's",
+      "Concerten",
+      "Zelf een instrument bespelen"
     ],
     "keywords": ["vrije tijd", "hobby", "hobbies"]
   },
   {
     "questions": [
-      "Had je een beste vriend(in)?",
-      "Wat deed je samen met je vrienden?"
+      "Beste vriend(in)",
+      "Activiteiten samen",
+      "Nieuwe vrienden leren kennen",
+      "Vrienden in kindertijd"
     ],
     "keywords": ["vrienden", "vriendschap", "vriend", "kennis", "kennissen"]
   },
   {
     "questions": [
-      "Herinner je je allereerste vakantie?",
-      "Wat is het verste land waar je al geweest bent?",
-      "Wanneer ging je voor het eerst alleen op vakantie? Waar was dat?",
-      "Op welke manier reisde je vooral?",
-      "Wat is de mooiste stad die je al hebt bezocht?"
+      "Eerste vakantie",
+      "Verre landen",
+      "Alleen op vakantie",
+      "Vervoersmiddelen",
+      "Mooie steden"
     ],
     "keywords": ["vakantie", "reizen", "reis", "avontuur"]
   },
   // MORE GENERAL
   {
     "questions": [
-      "Wie is je favoriete voetballer?",
-      "Beoefen je een sport?",
-      "Welke sport volg je graag?",
-      "Heb je ooit een wedstrijd gewonnen in een sport?"
+      "Favoriete voetballer",
+      "Zelf sporten",
+      "Favoriete sport",
+      "Wedstrijd gewonnen"
     ],
     "keywords": ["sport"]
   },
   {
     "questions": [
-      "Wat is je lievelingsgerecht?",
-      "Eet je soms iets exotisch?",
-      "Wat is je favoriete Italiaanse gerecht?",
-      "Welke Vlaamse kost is jouw favoriet?",
-      "Wat is jouw favoriet snoepgoed?",
+      "Lievelingsgerecht",
+      "Exotisch eten",
+      "Italiaanse gerechten",
+      "Vlaamse kost",
+      "Favoriet snoepgoed",
     ],
     "keywords": ["voeding", "eten"]
   },
   {
     "questions": [
-      "Ga je soms op café in je gemeente?",
-      "Herinner je je nog een buurtfeest?"
+      "Buurtcafés",
+      "Buurtfeesten",
+      "Feesten in gemeente",
+      "Evenementen in de gemeente",
     ],
     "keywords": ["gemeente", "roeselare"]
   },
