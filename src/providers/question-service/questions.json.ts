b612cffd9d7984012c46e0945978432858af2446
Thor Galle <thor.galle@student.kuleuven.be>
2017-08-02 12:53:35 +0200

    l -> L

--- src/providers/question-service/questions.json.ts
+++ src/providers/question-service/questions.json.ts
@@ -93,7 +93,7 @@ export const QUESTIONS = [
   },
   {
     "questions": [
-      "lievelingsgerecht",
+      "Lievelingsgerecht",
       "Exotisch eten",
       "Italiaanse gerechten",
       "Vlaamse kost",
