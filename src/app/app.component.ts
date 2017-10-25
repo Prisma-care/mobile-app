8bc0ec51e7ab67bdd0ce7cfb63419b3e8f00373a
Milenko Vorkapic <milenko@hackages.io>
2017-10-25 19:34:06 +0200

    Moved the sidebar to it's own component && autoreload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,34 @@
 import {Component, ViewChild, OnInit} from "@angular/core";
-import {MenuController, Nav, Platform} from "ionic-angular";
+import {MenuController, Nav, NavController, Platform} from "ionic-angular";
 import {StatusBar} from "@ionic-native/status-bar";
 import {SplashScreen} from "@ionic-native/splash-screen";
 
-import {PatientService} from "../providers/back-end/user.service";
-import {LoginPage} from "../pages/login/login";
-import {AuthService} from "../providers/auth-service/auth-service";
 import {TranslatorService} from "../providers/translator.service";
 import {TranslateService} from "@ngx-translate/core";
 import {InvitePage} from "../pages/invite/invite";
 import {StoryService} from "../providers/back-end/story.service";
 import {CURENT_VERSION, env} from "./environment";
-import {Mixpanel} from '@ionic-native/mixpanel';
 import {Analytics} from '../providers/analytics';
 import {AuthenticationPage} from './auth/authentication.component';
 import {AlbumsPage} from '../pages/albums/albums';
 import {AuthenticationService} from './core/authentication.service';
+import {PatientService} from "./core/patient.service";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp implements OnInit{
-  @ViewChild(Nav) nav: Nav;
-  rootPage: any = AuthenticationPage;
   private translate: TranslateService;
   private translator: TranslatorService;
 
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+  constructor(public platform: Platform, public splashScreen: SplashScreen,
               public patientService: PatientService, public translatorService: TranslatorService,
               public authService: AuthenticationService, public menu: MenuController,
               public storyService: StoryService,
-              private analytics: Analytics) {
+              private analytics: Analytics,
+              private statusBar: StatusBar
+              ) {
   }
 
   ngOnInit() {
@@ -42,28 +39,16 @@ export class MyApp implements OnInit{
 
     this.authService.autoLoad()
     if(this.authService.isLoggedIn()){
-      this.storyService.getAlbums(this.authService.getCurrentPatient().patient_id)
-      this.nav.setRoot(AlbumsPage)
+      this.storyService.getAlbums(this.patientService.getCurrentPatient().patient_id)
+      //this.nav.setRoot(AlbumsPage)
     }
-    localStorage.setItem(env.lastestUsedVersion, CURENT_VERSION);
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
-
     this.analytics.track('AppComponent::Prisma launched');
  }
 
-  logout() {
-    this.menu.close();
-    this.authService.logout();
-    this.nav.setRoot(LoginPage);
-  }
 
-  invite() {
-    this.menu.close();
-    this.nav.push(InvitePage, {
-      "patientId": this.authService.getCurrentPatient().patient_id
-    });
-  }
 }
---
+++ src/app/components/sidebar.component.ts
@@ -0,0 +1,56 @@
+
+import {Component, ViewChild} from "@angular/core";
+import {AuthenticationPage} from "../auth/authentication.component";
+import {MenuController, Nav} from "ionic-angular";
+import {AuthenticationService} from "../core/authentication.service";
+import {InvitePage} from "../../pages/invite/invite";
+import {PatientService} from "../core/patient.service";
+
+@Component({
+  selector: 'prisma-sidebar',
+  template: `<ion-menu [content]="content" color="white">
+    <ion-content>
+      <ion-toolbar color="white">
+        <ion-title style="color: #FFABAEB4">Menu</ion-title>
+      </ion-toolbar>
+      <ion-list>
+        <button ion-item (click)="invite()" *ngIf="patientService.getCurrentPatient()" class="ion-menu-buttons">
+          <ion-icon name="person-add" color="general"></ion-icon>
+          Nodig iemand uit
+        </button>
+        <button ion-item (click)="logout()" class="ion-menu-buttons">
+          <ion-icon name="exit" color="general"></ion-icon>
+          Afmelden
+        </button>
+      </ion-list>
+    </ion-content>
+  </ion-menu>
+
+  <ion-nav id="nav" #content #rootNavController [root]="rootPage" swipeBackEnabled="false">
+  </ion-nav>
+  `
+})
+export class SidebarComponent {
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = AuthenticationPage;
+
+
+  constructor(
+    public menu: MenuController,
+    public authService: AuthenticationService,
+    public patientService: PatientService) {
+  }
+
+  logout() {
+    this.menu.close();
+    this.authService.logout();
+    this.nav.setRoot(AuthenticationPage);
+  }
+
+  invite() {
+    this.menu.close();
+    this.nav.push(InvitePage, {
+      "patientId": this.patientService.getCurrentPatient().patient_id
+    });
+  }
+}
