3ae22339f6ce22c520563f0d71a8ade8e2d1ca8f
Thor Galle <thor.galle@student.kuleuven.be>
2017-11-01 16:39:35 +0100

    Initial fullstory integration

--- src/providers/analytics.ts
+++ src/providers/analytics.ts
@@ -42,6 +42,11 @@ export class Analytics {
   }
 
   identify(user: User): void {
+    this.mixpanelIdentify(user);
+    this.fullStoryIdentify(user);
+  }
+
+  mixpanelIdentify(user: User): void {
     // a function that handles the mixpanel registration
     let mixpanelIdentify: (() => void) =
       () => this.mixpanel.identify(String(user.id))
@@ -68,5 +73,91 @@ export class Analytics {
     else {
       this.init().then(mixpanelIdentify);
     }
+  }
+
+  /**
+    This exports the FS object from FullStory's tracking snippet
+  */
+  getFullStory(): any {
+    window['_fs_debug'] = false;
+    window['_fs_host'] = 'fullstory.com';
+    window['_fs_org'] = '8BAS6';
+    window['_fs_namespace'] = 'FS';
+
+    return (function(m, n, e, t, l, o, g, y) {
+            if (e in m) {
+                // >>>> CHANGED - return the existing fs if aldready existing
+                if (m[e].identify) {
+                  return m[e];
+                }
+                else {
+                  if (m.console && m.console.log) {
+                      m.console.log('FullStory was already loaded, but contains no identify function.');
+                  }
+                  return;
+                }
+
+                /*
+                if (m.console && m.console.log) {
+                    m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');
+                }
+                return;
+                */
+            }
+            g = m[e] = function(a, b) {					// export a bootstrap function to g and the namespace
+                g.q ? g.q.push([a, b]) : g._api(a, b);
+            };
+            g.q = [];
+
+            o = n.createElement(t); // create a new script element in window
+            o.async = 1;
+            o.src = 'https://' + window["_fs_host"] + '/s/fs.js';
+
+            y = n.getElementsByTagName(t)[0];
+            y.parentNode.insertBefore(o, y);
+
+            g.identify = function(i, v) { // create the identify function
+                g(l, {
+                    uid: i
+                });
+                if (v) g(l, v)
+            };
+
+            g.setUserVars = function(v) { // create the setUserVars function
+                g(l, v)
+            };
+
+            g.identifyAccount = function(i, v) { // create the identifyAccount function
+                o = 'account';
+                v = v || {};
+                v.acctId = i;
+                g(o, v)
+            };
+
+            g.clearUserCookie = function(c, d, i) { // create the clearUserCookie function
+                if (!c || document.cookie.match('fs_uid=[`;`]*`[`;`]*`[`;`]*`')) {
+                    d = n.domain;
+                    while (1) {
+                        n.cookie = 'fs_uid=;domain=' + d +
+                            ';path=/;expires=' + new Date(0).toUTCString();
+                        i = d.indexOf('.');
+                        if (i < 0) break;
+                        d = d.slice(i + 1)
+                    }
+                }
+            };
+
+            // ADDED - return the FS object
+            return g;
+
+        })(window, document, window['_fs_namespace'], 'script', 'user');
+  }
+
+  fullStoryIdentify(user: User) {
+    // This is an example script - don't forget to change it!
+    this.getFullStory().identify(String(user.id), {
+      displayName: `${user.firstName} ${user.lastName}`,
+      email: user.email
+    });
   }
 }
