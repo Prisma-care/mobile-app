125c06cd343364cf4dfa3a942438b755bea9f995
Victor <victor@hackages.io>
2017-11-03 00:29:03 +0100

    add intro page

---
+++ src/app/auth/components/intro/intro.component.ts
@@ -0,0 +1,35 @@
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular/navigation/nav-controller";
+import { AuthenticationPage } from "../../authentication.component";
+
+
+@Component({
+  selector: 'prisma-intro-page',
+  template: `
+    <ion-content>
+        <img src="assets/img/introPage/intro.jpg"/>
+        <h1>Kleur jullie <br/> herinneringen</h1>
+        <button ion-button full large (click)="onClick(false)">Registreer</button>
+        <p class="alternate-option" (click)="onClick(true)">
+         Al een account?
+        <a color="general">
+          Meld je aan.
+        </a>
+      </p>
+    </ion-content>
+
+  `,
+})
+export class IntroPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+  onClick(value:boolean){
+    this.navCtrl.push(AuthenticationPage, {
+      "isLogging":value
+    })
+  }
+
+}
