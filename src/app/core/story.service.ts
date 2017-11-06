6dd53bb3030da42093b6f8160119068e7501e1f9
Victor <victor@hackages.io>
2017-11-07 00:35:01 +0100

    refactor utilService in storyService

--- src/app/core/story.service.ts
+++ src/app/core/story.service.ts
@@ -1,84 +1,97 @@
 
-import {Inject, Injectable} from "@angular/core";
-import {Observable} from "rxjs/Observable";
-import {UserStory} from "../../dto/user-story";
-import {background, getMessageFromBackendError, getThumbnails, getUrlImage, youtubeId} from "../utils";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {Environment, EnvironmentToken} from "../environment";
+import { Inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { UserStory } from "../../dto/user-story";
+import { background, getMessageFromBackendError, getThumbnails, getUrlImage, youtubeId } from "../utils";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Environment, EnvironmentToken } from "../environment";
 import { Camera } from "@ionic-native/camera";
 import { FilePath } from "@ionic-native/file-path";
-import {File} from "@ionic-native/file";
-import { ToastController } from "ionic-angular";
+import { File } from "@ionic-native/file";
 
 interface storyResponse {
-  response:UserStory
+  response: UserStory
 }
 
 interface storiesResponse {
-  response:UserStory[]
+  response: UserStory[]
 }
 
-declare var cordova: any;
+interface youtubeResponse {
+  items: {
+    0: {
+      snippet: {
+        thumbnails: {
+          standard: {
+            url: string
+          }
+        }
+      }
+    }
+  }
+  pageInfo: {
+    totalResults: number
+  }
+}
 
 @Injectable()
 export class StoryService {
-  constructor(@Inject(EnvironmentToken) private env: Environment,
-              private http: HttpClient,
-              private camera: Camera,
-              private filePath: FilePath,
-              private file: File,
-              private toastCtrl: ToastController) {
+  constructor( @Inject(EnvironmentToken) private env: Environment,
+    private http: HttpClient,
+    private camera: Camera,
+    private filePath: FilePath,
+    private file: File) {
     this.handleError = this.handleError.bind(this);
   }
 
   getUserStory(patientId: string, storyId: string): Observable<UserStory | Error> {
 
     return this.http.get(`${this.env.apiUrl}/${this.env.api.getPatient}/${patientId}/${this.env.api.getStory}/${storyId}`)
-      .map(({response}:storyResponse) => new UserStory(response))
+      .map(({ response }: storyResponse) => new UserStory(response))
       .catch(err => this.handleError(err));
   }
 
-  getUserStories(): Observable<UserStory[]| Error >  {
+  getUserStories(): Observable<UserStory[] | Error> {
     return this.http.get("assets/json/stories.json")
-      .map(({response}:storiesResponse) => response.map(story => new UserStory(story)))
+      .map(({ response }: storiesResponse) => response.map(story => new UserStory(story)))
       .catch(error => this.handleError(error));
   }
 
-  addStory(patientId: number, newStory: UserStory): Observable<UserStory| Error >  {
+  addStory(patientId: number, newStory: UserStory): Observable<UserStory | Error> {
 
-    return this.http.post(`${this.env.apiUrl}/${this.env.api.getPatient}/${patientId}/${this.env.api.getStory}`,newStory)
-      .map(({response}:storyResponse) =>  new UserStory(response))
+    return this.http.post(`${this.env.apiUrl}/${this.env.api.getPatient}/${patientId}/${this.env.api.getStory}`, newStory)
+      .map(({ response }: storyResponse) => new UserStory(response))
       .catch(err => this.handleError(err));
   }
 
-  deleteStory(patientId: number, storyId: number): Observable<Object| Error >  {
+  deleteStory(patientId: number, storyId: number): Observable<Object | Error> {
     return this.http.delete(`${this.env.apiUrl}/${this.env.api.getPatient}/${patientId}/${this.env.api.getStory}/${storyId}`)
       .catch(err => this.handleError(err));
   }
 
-  updateStory(patientId: number, newStory: UserStory): Observable<UserStory| Error >  {
+  updateStory(patientId: number, newStory: UserStory): Observable<UserStory | Error> {
     return this.http.patch(`${this.env.apiUrl}/${this.env.api.getPatient}/${patientId}/${this.env.api.getStory}/${newStory.id}`, newStory)
-      .map(({response}:storyResponse) => new UserStory(response))
+      .map(({ response }: storyResponse) => new UserStory(response))
       .catch(err => this.handleError(err));
   }
 
-  getImage(filename: string): Observable<string| Error >  {
+  getImage(filename: string): Observable<string | Error> {
     return getUrlImage.call(this, filename)
   }
 
-  getYoutubeId(url:string):string {
+  getYoutubeId(url: string): string {
     return youtubeId(url)
   }
 
-  getThumb(url):string{
+  getThumb(url): string {
     return getThumbnails(url);
   }
 
-  getBackground(story: UserStory){
-    return background.call(this,story)
+  getBackground(story: UserStory): Observable<string | Error> {
+    return background.call(this, story)
   }
 
-  addYoutubeLinkAsset(patient_id: string, storyId: string, asset: string):Observable<Object|Error> {
+  addYoutubeLinkAsset(patient_id: number, storyId: number, asset: string): Observable<Object | Error> {
 
     return this.http.post(`${this.env.apiUrl}/${this.env.api.getPatient}/${patient_id}/${this.env.api.getStory}/${storyId}/${this.env.api.getAsset}`, {
       "asset": asset,
@@ -95,7 +108,7 @@ export class StoryService {
     return this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
   }
 
-  getPicture(sourceType):Observable<string> {
+  getPicture(sourceType): Observable<string> {
     const options = {
       quality: 90,
       sourceType: sourceType,
@@ -108,13 +121,13 @@ export class StoryService {
 
     return Observable.fromPromise(this.camera.getPicture(options))
       .map((imagePath) => {
-        if(sourceType === this.camera.PictureSourceType.PHOTOLIBRARY){
+        if (sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
           return Observable.fromPromise(this.filePath.resolveNativePath(imagePath))
-            .map((filePath)=>{
+            .map((filePath) => {
               let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
               let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
               return this.copyFileToLocalDir(correctPath, currentName)
-            }).switchMap(x=>x)
+            }).switchMap(x => x)
         } else {
           let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
           let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
@@ -123,9 +136,25 @@ export class StoryService {
       }).switchMap(x => x)
   }
 
-  copyFileToLocalDir(correctPath,currentName):Observable<string>{
-    return Observable.fromPromise(this.file.copyFile(correctPath, currentName, cordova.file.dataDirectory, `${new Date().getTime()}.jpg`))
+  copyFileToLocalDir(correctPath, currentName): Observable<string> {
+    return Observable.fromPromise(this.file.copyFile(correctPath, currentName, this.file.dataDirectory, `${new Date().getTime()}.jpg`))
       .map(file => file.name)
+  }
+
+  pathForImage(img) {
+    return this.file.dataDirectory + img;
+  }
+
+
+  checkYoutubeLink(url: string): Observable<string | null> {
+    const youtubeLinkRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+    if (url.toLowerCase().match(youtubeLinkRegex)) {
+      const urlId = this.getYoutubeId(url)
+      return this.http.get(`https://www.googleapis.com/youtube/v3/videos?id=${urlId}&key=${this.env.youtubeApiKey}&part=snippet`)
+        .map((res: youtubeResponse) => res.pageInfo.totalResults ? res.items[0].snippet.thumbnails.standard.url : null)
+    } else {
+      return Observable.of(null);
+    }
   }
 
   handleError(err: HttpErrorResponse): Observable<Error> {
