c4219a83f6c314c86096d4de1722cf1fe3f7fbf8
Victor <victor@hackages.io>
2017-11-09 17:53:19 +0100

    delete story and album component

--- src/app/storyList/story.module.ts
+++ src/app/storyList/story.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from "../shared/shared.module";
 import {StoryListPage} from "./storyList.component";
-import {StoryComponent} from "./story/story.component";
 import {QuestionComponent} from "./question/question.component";
 import {StoryDetailsPage} from "./story/storyDetail/storyDetail.component";
 import {StoryOptionsComponent} from "./story/storyDetail/component/storyOptions.component";
@@ -13,12 +12,11 @@ const IMPORTS = [
 ];
 const DECLARATIONS = [
   StoryListPage,
-  StoryComponent,
   StoryDetailsPage,
   StoryOptionsComponent,
   QuestionComponent,
   StoryListOptionsComponent,
-  createOrUpdateStoryPage
+  createOrUpdateStoryPage,
 ];
 
 @NgModule({
