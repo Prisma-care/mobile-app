d7eb16a2324ccb0ce4d83505e631311c0b64eb56
Victor <victor@hackages.io>
2017-11-13 15:22:15 +0100

    delete dto

--- src/dto/enum/story-type.ts
+++
@@ -1,6 +0,0 @@
-export enum StoryType {
-  TEXT = 0,
-  AUDIO = 1,
-  IMAGE = 2,
-  VIDEO = 3
-}
--- src/dto/heritage-story.ts
+++
@@ -1,5 +0,0 @@
-import {AbstractStory} from './abstract-story';
-
-export class HeritageStory extends AbstractStory {
-  // TODO: specific heritage properties
-}
