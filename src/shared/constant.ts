2d0c4a3b4a62dbabb3e178b49056dbe1a789d63f
Victor <victor@hackages.io>
2017-11-14 18:51:21 +0100

    new constant file

---
+++ src/shared/constant.ts
@@ -0,0 +1,35 @@
+import {environment} from '../environments/environment';
+import {Constant} from './types';
+
+export const constant: Constant = {
+  apiUrl: environment.apiUrl,
+  currentVersion: '0.3.2',
+  youtubeApiKey: environment.youtubeApiKey,
+  jwtToken: 'id_token',
+  lastestUsedVersion: 'version',
+  localstorage: {LOCALSTORAGE_SELECTEDLANG: 'langs'},
+  privateImagesRegex: '/asset/',
+  loadingImage: 'assets/img/homePage/loading.png',
+  emptyAlbum: 'assets/img/empty-album.png',
+  supportMailAddress: 'info@prisma.care',
+  api: {
+    getSignIn: 'signin',
+    getPatient: 'patient',
+    getAlbum: 'album',
+    getStory: 'story',
+    getAsset: 'asset',
+    getUser: 'user',
+    invite: 'invite'
+  },
+  temp: {
+    albums: 'albums',
+    currentUser: 'user',
+    currentPatient: 'patient'
+  },
+  methods: {
+    addNewStory: 'addNewStory',
+    addYoutubeStory: 'addYoutubeStory',
+    replaceDescription: 'replaceDescription',
+    replaceImage: 'replaceImage'
+  }
+};
